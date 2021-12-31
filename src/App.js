cdaee5d10128caf39c35154c92146a2d3278a509
Pannu786 <thepannu786@gmail.com>
2021-12-31 10:44:51 +0530

    Added Fragment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, Frgament, Fragment } from 'react';
 
 import AddUser from './components/Users/AddUser';
 import UsersList from './components/Users/UsersList';
@@ -16,11 +16,11 @@ function App() {
   }
 
   return (
-    <div>
+    <Fragment>
       <AddUser onAddUser={addUserHandler} />
       <UsersList users={usersList} />
-      helloooo
-    </div>
+    
+    </Fragment>
   );
 }
 
