899b00908adefc6cef223529050e4f566b2604cb
Pannu786 <thepannu786@gmail.com>
2021-12-31 22:02:14 +0530

    Added 'ref' instead of state

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 import ErrorModal from '../UI/ErrorModal';
 import Card from '../UI/Card';
@@ -7,37 +7,34 @@ import Button from '../UI/Button';
 import style from './AddUser.module.css';
 
 const AddUser = (props) => {
-  const [enteredUsername, setEnteredUsername] = useState('');
-  const [enteredAge, setEnteredAge] = useState('');
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
+
   const [error, setError] = useState();
 
   const addUserHandler = (e) => {
     e.preventDefault();
-    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+
+    const enteredName = nameInputRef.current.value;
+    const enteredUserAge = ageInputRef.current.value;
+
+    if (enteredName.trim().length === 0 || enteredUserAge.trim().length === 0) {
       setError({
         title: 'Invalid input!',
         msg: 'Please enter a valid username and age!',
       });
       return;
     }
-    if (+enteredAge < 1) {
+    if (+enteredUserAge < 1) {
       setError({
         title: 'Invalid Age!',
         msg: 'Please enter a valid age > 0',
       });
       return;
     }
-    props.onAddUser(enteredUsername, enteredAge);
-    setEnteredUsername('');
-    setEnteredAge('');
-  };
-
-  const usernameChangeHandler = (e) => {
-    setEnteredUsername(e.target.value);
-  };
-
-  const ageChangeHandler = (e) => {
-    setEnteredAge(e.target.value);
+    props.onAddUser(enteredName, enteredUserAge);
+    nameInputRef.current.value = '';
+    ageInputRef.current.value = '';
   };
 
   const errorHandler = () => {
@@ -45,23 +42,19 @@ const AddUser = (props) => {
   };
   return (
     <div>
-      {error && <ErrorModal title={error.title} msg={error.msg} onCustomclick={errorHandler}/>}
+      {error && (
+        <ErrorModal
+          title={error.title}
+          msg={error.msg}
+          onCustomclick={errorHandler}
+        />
+      )}
       <Card sexyLook={style.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor='username'>Username</label>
-          <input
-            id='username'
-            type='text'
-            value={enteredUsername}
-            onChange={usernameChangeHandler}
-          ></input>
+          <input id='username' type='text' ref={nameInputRef}></input>
           <label htmlFor='age'>Age (Year)</label>
-          <input
-            id='age'
-            type='number'
-            value={enteredAge}
-            onChange={ageChangeHandler}
-          ></input>
+          <input id='age' type='number' ref={ageInputRef}></input>
           <Button type='submit'>Add User</Button>
         </form>
       </Card>
